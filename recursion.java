0ea26dcfca982646c3d06d1bbf7887cc749db57d
almathaler <athaler00@stuy.edu>
2019-02-03 21:58:14 -0500

    main testing

--- recursion.java
+++ recursion.java
@@ -1,32 +1,67 @@
 import java.util.ArrayList;
-public class Recursion{
-    /*
-    public static void main(String[] args){
-      /*
-      for (int i = 0; i<100; i++){
-        System.out.println("fib(" + i + "): " + fib(i));
-      }
-      System.out.println("makeAllSums(3): ");
-      makeAllSums(3);
-      for (double i = 0; i<49; i+=.25){
-        System.out.println("sqrt(" + i + "): " + sqrt(i, .001));
-      }
-      */
-      //System.out.println("makeAllSums(3): " + makeAllSums(3));
-      //System.out.println("makeAllSums(20): " + makeAllSums(20));
-      //System.out.println("makeAllSums(7): " + makeAllSums(7));
-      //makeAllSums(3);
-      //for (double i = 0; i<=49; i+=.25){
-      //  System.out.println("sqrt(" + i + "): " + sqrt(i, .001));
-      //}
-    //}
-    /*You may write additional private methods */
+public class recursion{
+/*
+  public static void main(String[] args) {
+      System.out.println("sqrt()");
+      System.out.println("-----------------------\n");
 
-    /*Recursively find the sqrt using Newton's approximation
-     *tolerance is the allowed percent error the squared answer is away from n.
-     *precondition: n is non-negative
+      System.out.println("sqrt(4, 0.00001)");
+      System.out.println("EXPECTED: 2");
+      System.out.println(sqrt(4, 0.00001));         // 2
 
-    */
+      System.out.println("----");
+
+      System.out.println("sqrt(8, 0.00001)");
+      System.out.println("EXPECTED: ~2.8284");
+      System.out.println(sqrt(8, 0.00001));         // ~2.8284
+
+      System.out.println("----");
+
+      System.out.println("sqrt(0, 0.00001)");
+      System.out.println("EXPECTED: 0");
+      System.out.println(sqrt(0, 0.00001));         // 0
+
+      System.out.println("\n");
+
+      System.out.println("fib()");
+      System.out.println("-----------------------\n");
+
+      System.out.println("fib(0)");
+      System.out.println("EXPECTED: 0");
+      System.out.println(fib(0));                   // 0
+
+      System.out.println("----");
+
+      System.out.println("fib(1)");
+      System.out.println("EXPECTED: 1");
+      System.out.println(fib(1));                   // 1
+
+      System.out.println("----");
+
+      System.out.println("fib(7)");
+      System.out.println("EXPECTED: 13");
+      System.out.println(fib(7));                   // 13
+
+      System.out.println("makeAllSums()");
+      System.out.println("-----------------------\n");
+
+      System.out.println("makeAllSums(0)");
+      System.out.println("EXPECTED: [0]");
+      System.out.println(makeAllSums(0));           // [0]
+
+      System.out.println("----");
+
+      System.out.println("makeAllSums(1)");
+      System.out.println("EXPECTED: [1, 0]");
+      System.out.println(makeAllSums(1));           // [1, 0]
+
+      System.out.println("----");
+
+      System.out.println("makeAllSums(3)");
+      System.out.println("EXPECTED: [0, 3, 2, 5, 1, 4, 3, 6]");
+      System.out.println(makeAllSums(3));           // [0, 3, 2, 5, 1, 4, 3, 6]
+}
+*/
     public static double sqrt(double n, double tolerance){
       if (n == 0){
         return 0;
